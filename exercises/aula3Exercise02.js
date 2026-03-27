03aeea23cf7f6bfba250f5924b541a98cfbc88c3
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2026-03-27 15:55:41 -0300

    feat: resolve ex2 aula 3

--- exercises/aula3Exercise02.js
+++ exercises/aula3Exercise02.js
@@ -7,7 +7,7 @@ import {initRenderer,
         InfoBox,
         onWindowResize,
         createGroundPlaneXZ} from "../libs/util/util.js";
-
+import GUI from '../libs/util/dat.gui.module.js'
 let scene, renderer, camera, material, light, orbit;; // Initial variables
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // Init a basic renderer
@@ -23,38 +23,67 @@ window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)},
 let axesHelper = new THREE.AxesHelper( 12 );
 scene.add( axesHelper );
 
+let speed1 = 0.08;
+let speed2 = 0.05;
+
+let move1 = false;
+let move2 = false;
+
+function moveSphere1() {
+  if(sphere1.position.x < 8) {
+    sphere1.position.x += speed1;
+  }
+}
+
+function moveSphere2() {
+  if(sphere2.position.x < 8) {
+    sphere2.position.x += speed2;
+  }
+}
+
 // create the ground plane
 let plane = createGroundPlaneXZ(20, 20)
 scene.add(plane);
 
-// create a cube
-let cubeGeometry1 = new THREE.BoxGeometry(4, 4, 4);
-let cube1 = new THREE.Mesh(cubeGeometry1, material);
-let cubeGeometry2 = new THREE.BoxGeometry(2, 2, 2);
-let cube2 = new THREE.Mesh(cubeGeometry2, material);
-let cubeGeometry3 = new THREE.BoxGeometry(1, 1, 1);
-let cube3 = new THREE.Mesh(cubeGeometry3, material);
+let sphereGeometry = new THREE.SphereGeometry(1, 32, 16);
+let sphere1 = new THREE.Mesh(sphereGeometry, material);
+sphere1.position.set(-8, 1, -4);
+let sphere2 = new THREE.Mesh(sphereGeometry, material);
+sphere2.position.set(-8, 1, 4);
 
-// position the cubes
-cube1.position.set(0.0, 2.0, 0.0);
-cube2.position.set(-4.0, 1.0, 5.0);
-cube3.position.set(8.0, 0.5, 0.0);
-// add the cubes to the scene
-scene.add(cube1, cube2, cube3);
+scene.add(sphere1, sphere2);
 
-// Use this to show information onscreen
-let controls = new InfoBox();
-  controls.add("Basic Scene");
-  controls.addParagraph();
-  controls.add("Use mouse to interact:");
-  controls.add("* Left button to rotate");
-  controls.add("* Right button to translate (pan)");
-  controls.add("* Scroll to zoom in/out.");
-  controls.show();
 
+function buildInterface(){
+
+  var controls = new function() {
+    this.moverEsfera1 = function() {
+      move1 = true;
+    }
+    this.moverEsfera2 = function() {
+      move2 = true;
+    }
+    this.resetPosition = function() {
+      sphere1.position.set(-8, 1, -4);
+      sphere2.position.set(-8, 1, 4);
+      move1 = false;
+      move2 = false;
+    }
+  }
+  // GUI interface
+  let gui = new GUI();
+  gui.add(controls, 'moverEsfera1').name("Mover Esfera 1");
+  gui.add(controls, 'moverEsfera2').name("Mover Esfera 2");
+  gui.add(controls, 'resetPosition').name("Reset");
+}
+
+buildInterface();
 render();
+
 function render()
 {
+  if(move1) moveSphere1();
+  if(move2) moveSphere2();
   requestAnimationFrame(render);
   renderer.render(scene, camera) // Render scene
 }
