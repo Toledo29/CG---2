5e512293ca7f0b2300369202417142026c0d88f2
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2026-03-27 14:56:29 -0300

    feat: Adiciona exercícios aula 3 e começa ex1

---
+++ exercises/aula3Exercise01.js
@@ -0,0 +1,57 @@
+import * as THREE from  'three';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import {initRenderer, 
+        initCamera,
+        initDefaultBasicLight,
+        setDefaultMaterial,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneXZ} from "../libs/util/util.js";
+
+let scene, renderer, camera, material, light, orbit;; // Initial variables
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
+camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
+material = setDefaultMaterial(); // create a basic material
+light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// Show axes (parameter is size of each axis)
+let axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+let plane = createGroundPlaneXZ(20, 20)
+scene.add(plane);
+
+let sphereGeometry = new THREE.SphereGeometry(1, 32, 16);
+let sphere1 = new THREE.Mesh(sphereGeometry, material);
+sphere1.position.set(-8, 1, -4);
+let sphere2 = new THREE.Mesh(sphereGeometry, material);
+sphere2.position.set(-8, 1, 4);
+
+scene.add(sphere1, sphere2);
+
+
+
+// Use this to show information onscreen
+let controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+  controls.add("Esfera 1")
+  controls.add("Esfera 2")
+  controls.add("Reset")
+render();
+function render()
+{
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
