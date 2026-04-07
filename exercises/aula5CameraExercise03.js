66c58a96418fe052b030466ea7b3bf69a5b725fd
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2026-04-07 14:28:53 -0300

    feat: resolve ex 3 aula 5

--- exercises/aula5CameraExercise03.js
+++ exercises/aula5CameraExercise03.js
@@ -14,11 +14,32 @@ window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)},
 
 // Camera
 camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-   camera.position.set(0.0, 10.0, 25.0);
+   camera.position.set(0.0, 0.0, 0.0);
    // Para o exercício, a câmera deve estar na posição (0, 0, 0)
    camera.up.set( 0.0, 1.0, 0.0 );
    camera.lookAt(0.0, 0.0, 0.0);
 scene.add(camera)
+
+const lerpConfig1 = {
+  destination : new THREE.Vector3(0, 3, 25),
+  alpha: 0.05,
+  move: true,
+  angle:0
+}
+
+const lerpConfig2 = {
+  destination : new THREE.Vector3(15, 4, 13),
+  alpha: 0.05,
+  move: false,
+  angle:45
+}
+
+const lerpConfig3 = {
+  destination : new THREE.Vector3(20, 6, 0),
+  alpha: 0.05,
+  move: false,
+  angle:90
+}
 
 buildScene();
 buildInterface();
@@ -26,6 +47,24 @@ render();
 
 function render()
 {
+  if(lerpConfig1.move){
+    let rad = THREE.MathUtils.degToRad(lerpConfig1.angle);
+    let quat = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), rad);
+    camera.position.lerp(lerpConfig1.destination, lerpConfig1.alpha);
+    camera.quaternion.slerp(quat, lerpConfig1.alpha);
+  }
+  if(lerpConfig2.move){
+    let rad = THREE.MathUtils.degToRad(lerpConfig2.angle);
+    let quat = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), rad);
+    camera.position.lerp(lerpConfig2.destination, lerpConfig2.alpha);
+    camera.quaternion.slerp(quat, lerpConfig2.alpha);
+  }
+  if(lerpConfig3.move){
+    let rad = THREE.MathUtils.degToRad(lerpConfig3.angle);
+    let quat = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), rad);
+    camera.position.lerp(lerpConfig3.destination, lerpConfig3.alpha);
+    camera.quaternion.slerp(quat, lerpConfig3.alpha);
+  }
    requestAnimationFrame(render);
    renderer.render(scene, camera) // Render scene
 }
@@ -36,13 +75,19 @@ function buildInterface()
   var controls = new function ()
   {
     this.movePosition1 = function(){
-      console.log("Este botão ainda não faz nada - 1");
+      lerpConfig1.move = true;
+      lerpConfig2.move = false;
+      lerpConfig3.move = false;
     };
     this.movePosition2 = function(){
-      console.log("Este botão ainda não faz nada - 2");
+      lerpConfig2.move = true;
+      lerpConfig1.move = false;
+      lerpConfig3.move = false;
     };
     this.movePosition3 = function(){
-      console.log("Este botão ainda não faz nada - 3");
+      lerpConfig3.move = true;
+      lerpConfig1.move = false;
+      lerpConfig2.move = false;
     };        
   };
 
