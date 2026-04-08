472faea523fef076172a8feb1d4ed1497296da43
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2026-04-08 15:10:20 -0300

    feat: adiciona plano de exemplo e posiciona a camera

--- T1/jogo.js
+++ T1/jogo.js
@@ -6,6 +6,31 @@ import {initRenderer,
         setDefaultMaterial,
         InfoBox,
         onWindowResize,
-        createGroundPlaneXZ} from "../libs/util/util.js";
+        createGroundPlaneWired} from "../libs/util/util.js";
+
 let scene, renderer, camera, material, light, orbit;; // Initial variables
-scene = new THREE.Scene();
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
+camera = initCamera(new THREE.Vector3(0, 5, 25)); // Init camera in this position
+material = setDefaultMaterial(); // create a basic material
+light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// Show axes (parameter is size of each axis)
+let axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+let plane = createGroundPlaneWired(40, 40)
+scene.add(plane);
+
+
+render();
+function render()
+{
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
