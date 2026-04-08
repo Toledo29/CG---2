75e50ebb533c5bc9a7ad25d42ae89083780bbcb0
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2026-04-08 14:39:53 -0300

    feat: cria pasta do trabalho e adiciona documentos htlm e js basicos

---
+++ T1/jogo.js
@@ -0,0 +1,11 @@
+import * as THREE from  'three';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import {initRenderer, 
+        initCamera,
+        initDefaultBasicLight,
+        setDefaultMaterial,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneXZ} from "../libs/util/util.js";
+let scene, renderer, camera, material, light, orbit;; // Initial variables
+scene = new THREE.Scene();
