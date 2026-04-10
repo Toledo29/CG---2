8b0ab0f3856127f911004cbcd382ef8f1d3a7ecf
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2026-04-10 14:58:31 -0300

    feat: adiciona nova disposição das árvores e corrige o posicionamento da camera

--- T1/jogo.js
+++ T1/jogo.js
@@ -11,9 +11,10 @@ import {initRenderer,
 let scene, renderer, camera, light, orbit;; // Initial variables
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // Init a basic renderer
-camera = initCamera(new THREE.Vector3(0, 5, -25)); // Init camera in this position
+camera = initCamera(new THREE.Vector3(0, 11, -40)); // Init camera in this position
+camera.lookAt(0, 11, 0);
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
-orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+// orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
 
 // cria os materiais para os objetos
 let material1 = setDefaultMaterial('rgb(139, 69, 19)');
@@ -25,26 +26,27 @@ window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)},
 
 
 // cria plano do chão
-let plane = createGroundPlaneWired(40, 40)
+let plane = createGroundPlaneWired(50, 50)
 scene.add(plane);
 
 // cria geometria dos componentes da árvore
-let logGeometry1 = new THREE.CylinderGeometry(0.5, 0.5, 5, 32);
-let logGeometry2 = new THREE.CylinderGeometry(0.5, 0.5, 5, 32);
-let sphereLeafGeometry1 = new THREE.SphereGeometry(2, 32, 32);
-let coneLeafGeometry1 = new THREE.ConeGeometry(3, 2, 32);
-let coneLeafGeometry2 = new THREE.ConeGeometry(2.5, 2, 32);
-let coneLeafGeometry3 = new THREE.ConeGeometry(2, 2, 32);
+let logGeometry1 = new THREE.CylinderGeometry(0.3, 0.3, 3, 32);
+let logGeometry2 = new THREE.CylinderGeometry(0.3, 0.3, 3, 32);
+let sphereLeafGeometry1 = new THREE.SphereGeometry(1.3, 32, 32);
+let coneLeafGeometry1 = new THREE.ConeGeometry(2, 2, 32);
+let coneLeafGeometry2 = new THREE.ConeGeometry(1.5, 2, 32);
+let coneLeafGeometry3 = new THREE.ConeGeometry(1, 2, 32);
 
 // cria componentes da árvore e posiciona eles
-for(let i = 0; i < 2; i++){
-  for(let j = 0; j < 8; j++){
-    if(j % 2 == 0){
+for(let i = 0; i < 12; i++){
+  for(let j = 0; j < 10; j++){
+    let num = Math.floor(Math.random() * 100);
+    if(num % 2 == 0){
       let log = new THREE.Mesh(logGeometry1, material1);
       let sphereleaf = new THREE.Mesh(sphereLeafGeometry1, material2);
-      log.position.set(10-(20*i), 2.5, j*5-17);
+      log.position.set(22-(i*4), 1.5, j*5-22);
       log.add(sphereleaf);
-      sphereleaf.position.set(0, 2.5, 0);
+      sphereleaf.position.set(0, 1.5, 0);
       scene.add(log);
     }
     else{
@@ -52,7 +54,7 @@ for(let i = 0; i < 2; i++){
       let coneleaf1 = new THREE.Mesh(coneLeafGeometry1, material3);
       let coneleaf2 = new THREE.Mesh(coneLeafGeometry2, material3);
       let coneleaf3 = new THREE.Mesh(coneLeafGeometry3, material3);
-      log.position.set(10-(20*i), 2.5, j*5-17);
+      log.position.set(22-(4*i), 1.5, j*5-22);
       log.add(coneleaf1);
       log.add(coneleaf2);
       log.add(coneleaf3);
